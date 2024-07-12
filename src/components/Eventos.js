f5d26b9f2bb4cd99174806700c66b498f11b62ed
Miguel Da Silva <infotecjuniorr@gmail.com>
2024-07-13 00:16:11 +0100

    Create Eventos.js

---
+++ src/components/Eventos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Eventos = () => {
+  return (
+    <div>
+      Página Eventos
+    </div>
+  );
+};
+
+export default Eventos;
