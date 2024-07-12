70dcdd48289c10e904c6ae5f4c5c02c6ce57cb76
Vicente <165604764+mvr2004@users.noreply.github.com>
2024-07-12 23:48:18 +0100

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,48 +1,27 @@
 import React from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-    return (
-        <div>
-            {/* AZUL */}
-            <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: 'black' }}>
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src="./images/logo-softinsa.png" alt="logo softinsa" width="100" />
-                    </a>
-                    <a className="navbar-brand" href="#!">Dashboard</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarText">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#!">Features</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <span className="navbar-text">
-                            Navbar text with an inline element
-                        </span>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Softinsa</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/users">User Management</Link>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
