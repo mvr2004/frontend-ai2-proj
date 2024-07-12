2d3989ab31bf5a638eb39e74cac78494a6644e1d
Miguel Da Silva <infotecjuniorr@gmail.com>
2024-07-13 00:52:15 +0100

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [nome, setNome] = useState('');
