fc5bfb7061a64b3ea9f9e5b917cfd44a4f196ed3
Vicente <165604764+mvr2004@users.noreply.github.com>
2024-07-12 23:47:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
+import UserManagement from './pages/UserManagement';
+import Navigation from './components/Navigation';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        {/* Adicione outras rotas aqui, por exemplo, uma rota para o dashboard */}
-      </Routes>
+      <Navigation />
+      <div className="container mt-5">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/users" element={<UserManagement />} />
+          {/* Adicione outras rotas aqui, por exemplo, uma rota para o dashboard */}
+        </Routes>
+      </div>
     </Router>
   );
 };
