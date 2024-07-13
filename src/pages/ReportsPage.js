93685897b7dfdc2fa329210ddcf9831291fad28f
Miguel Da Silva <infotecjuniorr@gmail.com>
2024-07-13 04:04:14 +0100

    reports page

---
+++ src/pages/ReportsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReportsList from '../views/listar_Reports';
+
+const ReportsPage = () => {
+  return (
+    <div>
+      <ReportsList />
+    </div>
+  );
+};
+
+export default ReportsPage;
