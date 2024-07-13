250a82ca7a12977fe331eae7fcd49ec28b777d2b
Miguel Da Silva <infotecjuniorr@gmail.com>
2024-07-13 04:05:44 +0100

    Update Navigation.js
    Adicionado o dropdown dos Reportes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Softinsa</Link>
+        <Link className="navbar-brand" to="#!">Softinsa</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -16,6 +16,15 @@ const Navigation = () => {
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/users">User Management</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Reports
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="/reportsPorResolver">To Solve</a></li>
+                <li><a className="dropdown-item" href="reportsResolvidos">Solved</a></li>
+              </ul>
             </li>
           </ul>
         </div>
