33cfde18a4dcd6e30625135354413920846d6af0
Miguel Da Silva <infotecjuniorr@gmail.com>
2024-07-13 04:04:55 +0100

    Update App.js
    Adicionado os routes dos Reportes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import UserManagement from './views/listar_User';
-import Navigation from './components/Navigation'; 
+import Navigation from './components/Navigation';
+import ReportsList from './views/listar_Reports';
 
 const App = () => {
   return (
@@ -16,6 +17,8 @@ const App = () => {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/users" element={<UserManagement />} />
+          <Route path="/reportsPorResolver" element={<ReportsList resolvido={false} />} />
+          <Route path="/reportsResolvidos" element={<ReportsList resolvido={true} />} />
         </Routes>
       </div>
     </Router>
